1d5d738af7911f0228a018e1843d504fcd1834bf
zachkaufman36 <35308366+zachkaufman36@users.noreply.github.com>
2024-11-27 14:42:26 -0500

    Adjusted SQL statement to make model predict future year

--- submit/datainjectionFromAWS.py
+++ submit/datainjectionFromAWS.py
@@ -11,37 +11,20 @@ database_name = 'fantasyfootballproject'
 # Function to generate SQL query for a specific year
 def generate_query(year):
     return f"""
-    WITH articles (playerId, avgArticleScore, season) AS (
-        SELECT 
-            playerId, 
-            AVG(articleScore) as avgArticleScore, 
-            YEAR(date) AS season 
-        FROM 
-            fantasyfootballproject.articles 
-        GROUP BY 
-            playerId, season
-    )
-    SELECT 
-        playerName, pid, position, passing.year, height, weight, age, injuries, adp, totalPoints, 
-        passingYards, attempts, completions, interceptions, sacks, passingTDs, passing2Pts, 
-        receptions, targets, receivingYards, receivingTDs, receiving2Pts, rushingYards, 
-        carries, rushingTDs, rushing2Pts, fumbles, avgArticleScore 
-    FROM 
-        fantasyfootballproject.players p
-    INNER JOIN 
-        fantasyfootballproject.passing passing ON (p.pid = passing.playerId)
-    INNER JOIN 
-        fantasyfootballproject.receiving rec ON (passing.playerId = rec.playerId AND passing.year = rec.year)
-    INNER JOIN 
-        fantasyfootballproject.rushing rush ON (rec.playerId = rush.playerId AND rec.year = rush.year)
-    INNER JOIN 
-        fantasyfootballproject.fumbles f ON (f.playerId = rush.playerId AND f.year = rush.year)
-    INNER JOIN 
-        fantasyfootballproject.playerRankingByYear prby ON (passing.playerId = prby.playerId AND passing.year = prby.year)
-    LEFT JOIN 
-        articles a ON (a.playerId = prby.playerId AND passing.year = a.season)
-    WHERE 
-        passing.year = {year};
+    WITH articles (playerId, avgArticleScore, season) AS (SELECT playerId, AVG(articleScore) as avgArticleScore, YEAR(date) 
+    AS season FROM fantasyfootballproject.articles GROUP BY playerId, season) 
+    
+    SELECT playerName, pid, posid AS position, passing.year as prevSeason, height, weight, age, injuries, adp, totalPoints, 
+    passingYards, attempts, completions, interceptions, sacks, passingTDs, passing2Pts, receptions, targets, receivingYards, 
+    receivingTDs, receiving2Pts, rushingYards, carries, rushingTDs, rushing2Pts, fumbles, avgArticleScore 
+    FROM fantasyfootballproject.players p 
+    INNER JOIN fantasyfootballproject.positions pos ON (p.position = pos.posid) 
+    INNER JOIN fantasyfootballproject.passing passing ON (p.pid = passing.playerId) 
+    INNER JOIN fantasyfootballproject.receiving rec ON (passing.playerId = rec.playerId AND passing.year = rec.year) 
+    INNER JOIN fantasyfootballproject.rushing rush ON (rec.playerId = rush.playerId AND rec.year = rush.year) 
+    INNER JOIN fantasyfootballproject.fumbles f ON (f.playerId = rush.playerId AND f.year = rush.year) 
+    INNER JOIN fantasyfootballproject.playerRankingByYear prby ON (passing.playerId = prby.playerId AND passing.year = prby.year - 1)
+    LEFT JOIN articles a ON (a.playerId = prby.playerId AND passing.year = a.season - 1) WHERE passing.year = {year - 1};
     """
 
 # Connect to the database and fetch data
